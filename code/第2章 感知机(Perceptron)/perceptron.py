011b1d22369f2d0ea690f70d0e0f189b9f8d3cbb
Zhang Zhe <38390381+Zhang3zhe@users.noreply.github.com>
2019-04-04 12:06:23 +0800

    Create perceptron.py

---
+++ code/第2章 感知机(Perceptron)/perceptron.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+class Model:
+    def __init__(self, length):
+        self.w = np.ones(length-1, dtype = np.float32)
+        self.b = 0
+        self.l_rate = 0.1
+
+    def sign(self, x, w, b):
+        y = np.dot(x, w) + b
+        return y
+
+    # 随机梯度下降法
+    def fit(self, X_train, y_train):
+        is_wrong = False
+        while not is_wrong:
+            wrong_count = 0
+            for d in range(len(X_train)):
+                X = X_train[d]
+                y = y_train[d]
+                if y * self.sign(X, self.w, self.b) <= 0:
+                    self.w = self.w + self.l_rate * np.dot(y, X)
+                    self.b = self.b + self.l_rate * y
+                    wrong_count += 1
+            if wrong_count == 0:
+                is_wrong = True
+        return 'Percertron Model!'
